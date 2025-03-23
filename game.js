d9aa4a662bc00076bba7636f8032d623bc9e5bc7
Nancy Chen <nancyc85@nycstudents.net>
2025-03-22 22:22:29 -0400

    add new characters img & edit code

--- game.js
+++ game.js
@@ -5,17 +5,17 @@ class StartScene extends Phaser.Scene {
     }
     preload() {
         this.load.image("startBg", "assets/startbackground.jpeg"); // Load home background
+        this.load.image("button", "assets/playbutton.png"); // Load button
     }
     create() {
         this.add.image(0, 0, "startBg").setOrigin(0, 0).setDisplaySize(this.scale.width, this.scale.height).setScale(1); // Set background image
-
         // Create an "Instructions" button
-        const instructionsButton = this.add.image(this.scale.width / 2, this.scale.height / 2, 'button' )
+        const instructionsButton = this.add.image(this.scale.width / 2, this.scale.height / 2, 'button')
+            .setScale(0.75)
             .setInteractive()
             .on("pointerdown", () => {
                 this.scene.start("InstructionScene"); // Switch to InstructionScene
             });
-
     }
 }
 
@@ -26,13 +26,23 @@ class InstructionScene extends Phaser.Scene {
     }
     preload() {
         this.load.image("instructionsBg", "assets/peachpuffbg.png"); // Load home background
+        this.load.image("start", "assets/start.jpg"); // Load button
     }
     create() {
         this.add.image(612, 598, "instructionsBg").setScale(1); // Set background image
-        this.add.text(200, 100, "Game Instructions", { fontSize: "24px", fill: "black" });
-        this.add.text(200, 100, "Use arrow keys to move \n Use SPACE bar to jump \n Match your commands to the \n beat of the music \n Solve math problems \n to avoid obstacles \n If you miss a beat/obstacle, \n it is game over \n Get through all the obstacles and \n match the beats to win the game!", { fontSize: "24px", fill: "black" });
+        this.add.text(50, 50, "Game Instructions", { fontSize: "23.5px", fill: "black" });
+        this.add.text(70, 100, "Use arrow keys to move", { fontSize: "23px", fill: "black" });
+        this.add.text(70, 150, "Use SPACE bar to jump", { fontSize: "23px", fill: "black" });
+        this.add.text(70, 200, "Match your commands to the beat of", { fontSize: "23px", fill: "black" });
+        this.add.text(70, 220, "the music", { fontSize: "23px", fill: "black" });
+        this.add.text(70, 270, "Solve math problems to avoid obstacles", { fontSize: "23px", fill: "black" });
+        this.add.text(70, 320, "If you miss a beat/obstacle, it is", { fontSize: "23px", fill: "black" });
+        this.add.text(70, 340, "game over", { fontSize: "23px", fill: "black" });
+        this.add.text(70, 390, "Get through all the obstacles", { fontSize: "23px", fill: "black" });
+        this.add.text(70, 440, "Match the beats to win the game!", { fontSize: "23px", fill: "black" });
 
-        const startButton = this.add.text(220, 200, "Start", { fontSize: "32px", fill: "black" })
+        const startButton = this.add.image(150, 530, 'start')
+        .setScale(0.20)
             .setInteractive()
             .on("pointerdown", () => {
                 this.scene.start("HomeScene");
@@ -50,11 +60,11 @@ class HomeScene extends Phaser.Scene {
     }
     create() {
         this.add.image(612, 598, "homeBg").setScale(1); // Set background image
-        this.add.text(150, 50, "Select a Level", { fontSize: "24px", fill: "black" });
+        this.add.text(50, 50, "Select a Level", { fontSize: "35px", fill: "black" });
 
         const levels = ["Level1", "Level2", "Level3", "Level4", "Level5"];
         levels.forEach((level, index) => {
-            this.add.text(200, 150 + index * 50, level, { fontSize: "32px", fill: "black" })
+            this.add.text(70, 150 + index * 50, level, { fontSize: "32px", fill: "black" })
                 .setInteractive()
                 .on("pointerdown", () => {
                     this.scene.start(level);
